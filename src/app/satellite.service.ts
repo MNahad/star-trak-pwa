9700527bc6abb21da16bfd1a61767306ed652bb8
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2021-10-10 21:44:06 +0100

    Make Satellite Service use env-defined GP dataset URL

--- src/app/satellite.service.ts
+++ src/app/satellite.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { fromEvent } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,22 @@ export class SatelliteService {
   tracker$ = fromEvent<MessageEvent<SatelliteData>>(this.tracker, 'message');
 
   startTracker({ observer: { lat_deg, lon_deg, alt_km }, period }: TrackerConfig): void {
-    fetch("../assets/wasm/data.json")
-      .then(res => res.json())
+    fetch(environment.gpUrl)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return Promise.reject();
+        }
+      })
       .then(gpElements => {
         this.tracker.postMessage({
           gpElements,
           coords: [lat_deg, lon_deg, alt_km],
           period,
         });
-      });
+      })
+      .catch(() => {});
   }
 
   updateObserver({ lat_deg, lon_deg, alt_km }: TrackerConfig["observer"]): void {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  gpUrl: "http://localhost:5000/gp" // CHANGE ME
 };
